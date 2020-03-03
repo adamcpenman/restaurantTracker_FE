3c389251de5f25f9851657e638d46e3404714367
Adam <adamcpenman@gmail.com>
2020-03-03 15:59:34 -0600

    merge

---
+++ src/Components/RestData.js
@@ -0,0 +1,34 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios"
+import RestCard from "./RestCard"
+
+function RestData() {
+    const [rest, setRest] = useState([]);
+
+    useEffect(() => {
+        const getRestData = () => {
+            axios  
+                .get("http://localhost:4000/restaurants")
+                .then(response => {
+                    setRest(response.data);
+                    console.log(response.data)
+                })
+                .catch(error => {
+                    console.log(error)
+                })
+        }
+
+        getRestData();
+    }, [])
+    return (
+        <div>
+            <section>
+                {rest.map(restaurant => (
+                    <RestCard key={restaurant.name} restaurant={restaurant} />
+                ))}
+            </section>
+        </div>
+    )
+}
+
+export default RestData
